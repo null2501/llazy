1e6c3d5b2d5a8269773920cd66180a72a0cea535
Null2501 <andrea.null404@gmail.com>
2020-01-09 14:17:23 +0100

    llazy is the new lazy

--- llazy.js
+++ llazy.js
@@ -1,5 +1,5 @@
 ;(function(global){
-  global.$lazy = global.$lazy || new function(){
+  global.$llazy = global.$lazy || new function(){
     var self = this, observer = false;
     self.update = function(obj){
       obj = obj || global.document;
